193d41130e020a642bd976e87d0246afb2bc235f
damian <damian@LAB2-MAESTRO>
2023-04-26 12:20:27 -0600

    Agregar archivos de proyecto.

---
+++ Segunda4E/Shared/Modelos/Producto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Segunda4E.Shared.Modelos
+{
+    public class Producto
+    {
+        [Range(1, int.MaxValue, ErrorMessage ="El número de inventario debe ser entero positivo")]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Debe escribir el nombre")]
+        public string? Nombre { get; set; }
+        [Range(1,3,ErrorMessage ="Debe seleccionar departamento")]
+        public int Departamento { get; set; }
+    }
+}
